c2834598e03edb679eaee524d041f36e2ef05656
gustavoghp87 <ghp.2120@gmail.com>
2020-10-22 14:43:43 -0300

    separo bot7200

--- bot-args/favretweet.py
+++ bot-args/favretweet.py
@@ -1,14 +1,7 @@
 from logging import log
-import requests
 import tweepy
 import logging
 from config import create_api
-import time
-import json
-from decouple import config
-#import sys
-#import os
-#import requests
 
 
 logging.basicConfig(level=logging.INFO)
@@ -18,34 +11,6 @@ class FavRetweetListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
         self.me = api.me()
-
-        starttime = time.time()
-        while True:
-            print(f"tick, son las {time.asctime(time.localtime(time.time()))}")
-            url = 'https://maslabook.herokuapp.com/api/bot'
-            payload = {'password': config('COUNTER_PW')}
-            headers = {'content-type': 'application/json'}
-            tuit = requests.post(url, data=json.dumps(payload), headers=headers)
-            print(tuit.json())
-            self.api.update_status(tuit.json())
-            
-            friends_names = []
-            for friend in api.friends():
-                friends_names.append(friend.screen_name)
-
-            #print("Yo sigo:", friends_names)
-
-            for follower in api.followers():
-                if follower.screen_name not in friends_names:
-                    try:
-                        follower.follow()
-                        api.create_mute(follower.screen_name)
-                        print (f"Siguiendo a {follower.screen_name}, silenciado")
-                    except:
-                        print("\n")
-
-            loop = 7200
-            time.sleep(loop - ((time.time() - starttime) % loop))
 
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
@@ -103,7 +68,6 @@ def main():
     api = create_api()
 
     keywords = ["barranis", "barrani", "Barrani", "maslazoom", "maslazooms", "MaslaZoom", "Maslazoom", "MASLAZOOM"]
-    #keywords = ["cochinchina"]
     tweets_listener = FavRetweetListener(api)
     stream = tweepy.Stream(api.auth, tweets_listener)
     stream.filter(track=keywords, languages=["es"])
